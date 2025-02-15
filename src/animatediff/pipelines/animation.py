6af87f2912fe04f7b644c610793c6683dd60bc11
chaowenguo <chaowen.guo1@gmail.com>
2025-02-14 18:20:12 -0800

    Update animation.py

--- src/animatediff/pipelines/animation.py
+++ src/animatediff/pipelines/animation.py
@@ -3457,21 +3457,6 @@ class AnimationPipeline(DiffusionPipeline, TextualInversionLoaderMixin):
 
         return AnimationPipelineOutput(videos=video)
 
-    def progress_bar(self, iterable=None, total=None):
-        if not hasattr(self, "_progress_bar_config"):
-            self._progress_bar_config = {}
-        elif not isinstance(self._progress_bar_config, dict):
-            raise ValueError(
-                f"`self._progress_bar_config` should be of type `dict`, but is {type(self._progress_bar_config)}."
-            )
-
-        if iterable is not None:
-            return tqdm(iterable, **self._progress_bar_config)
-        elif total is not None:
-            return tqdm(total=total, **self._progress_bar_config)
-        else:
-            raise ValueError("Either `total` or `iterable` has to be defined.")
-
     def freeze(self):
         logger.debug("Freezing pipeline...")
         _ = self.unet.eval()
